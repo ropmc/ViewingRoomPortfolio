f050eaa35270b08d6700472c3254df03335717f6
Rodrigo <ropenteadomc@gmail.com>
2023-05-15 12:18:43 -0300

    Teste

---
+++ Experience/Utils/Assets.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        name: 'room',
+        type: 'glbModel',
+        path: '/models/NewArtGallery.glb'
+    },
+    {
+        name: 'tela1',
+        type: 'videoTexture',
+        path: '/textures/Tela1.mp4'
+    },
+
+    {
+        name: 'tela2',
+        type: 'videoTexture',
+        path: '/textures/Tela2.mp4'
+    },
+    
+    {
+        name: 'tela3',
+        type: 'videoTexture',
+        path: '/textures/Tela3.mp4'
+    },
+
+    {
+        name: 'tela4',
+        type: 'videoTexture',
+        path: '/textures/Tela4.mp4'
+    },
+
+    {
+        name: 'tela5',
+        type: 'videoTexture',
+        path: '/textures/Tela5.mp4'
+    },
+
+    {
+        name: 'tela6',
+        type: 'videoTexture',
+        path: '/textures/Strange Format.mp4'
+    },
+
+    {
+        name: 'tela7',
+        type: 'videoTexture',
+        path: '/textures/Tela7.mp4'
+    },
+
+    {
+        name: 'tela8',
+        type: 'videoTexture',
+        path: '/textures/Tela8.mp4'
+    },
+]
